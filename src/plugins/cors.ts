b6229bd876a851b6bc6beeca21c2f286f241094c
Abdullah <a.maher.bina@gmail.com>
2024-11-26 15:23:51 +0300

    update cors

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -4,6 +4,7 @@ import cors, { FastifyCorsOptions } from "@fastify/cors";
 export default fp<FastifyCorsOptions>(
   async (fastify) => {
     fastify.register(cors, {
+      origin: ["http://localhost:3000", "https://wslah-ui.fly.dev"],
       exposedHeaders: ["x-total-count", "x-total-pages", "x-current-page"],
     });
   },
